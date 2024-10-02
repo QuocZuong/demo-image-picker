ee3df745d2bc65dd4a15b176f068f531442abad7
Quoc Vuong <quocvuongle.ct@gmail.com>
2024-10-02 19:05:03 +0700

    Initial commit

--- App.js
+++ App.js
@@ -1,10 +1,46 @@
+import {useState, useEffect} from "react";
+import * as ImagePicker from "expo-image-picker";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
+  
+  const [image, setImage] = useState(null);
+
+  const [hasPermission, setHasPermission] = useState(null);
+
+ useEffect(() => {
+    (async () => {
+     const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync(); 
+
+     if(status === "granted") {
+      setHasPermission(true);
+     }
+    })();
+ }, [])
+
+  const pickImage = async () => {
+
+    if(!hasPermission) {
+      alert("Permission is required to access the library");
+      return;
+    }
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1,
+    })
+
+    if(!result.canceled)  {
+      setImage(result.assets[0].uri);
+    }
+  } 
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Pick an image" onPress={pickImage}/>
+      <Image source={{uri: image}} style={{width: 200, height: 200}}/>
       <StatusBar style="auto" />
     </View>
   );
